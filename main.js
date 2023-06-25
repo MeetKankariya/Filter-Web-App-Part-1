a9c7d8780858a47256d0c4269d2589a4233071a5
MeetKankariya <105592761+MeetKankariya@users.noreply.github.com>
2023-06-25 13:04:21 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function preload(){
+
+}
+
+function setup(){
+    canvas=createCanvas(325,325)
+    canvas.center()
+}
+
+function draw(){
+
+}
+
+function click(){
+    save ('My Selfiee.png')
+}
